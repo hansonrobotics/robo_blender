1042b27785491cff4abc3d575bf8a9c38b39a5ea
Ean Schuessler <ean@brainfood.com>
2014-05-30 17:49:40 +0800

    External drivers and text configs
    Motor configuration and ROS inputs are now configured through
    external YAML files. ROS processors can also be defined in external
    files to keep the complexity of robo.py down.

---
+++ src/processors/faceshift_source.py
@@ -0,0 +1,20 @@
+import mathutils
+from ros_faceshift.msg import fsMsgTrackingState
+
+class faceshift_source:
+    def process(self, msg, config):
+        binding = config["source"].split(":")
+        #print("MSG %s" % msg.m_headRotation)
+        field = getattr(msg, binding[1])
+
+        if binding[1] == "m_coeffs":
+            element = field[int(binding[2])]
+            print("%s" % element)
+            return element
+
+        element = getattr(field, binding[2])
+        return element
+    def msgclass(self):
+        return fsMsgTrackingState
+
+processors["faceshift_source"] = faceshift_source()
--- src/robo.py
+++ src/robo.py
@@ -5,6 +5,7 @@ import bpy
 import math
 import threading
 import yaml
+import importlib
 
 from mathutils import Matrix, Vector
 from math import acos, degrees
@@ -14,50 +15,98 @@ from std_msgs.msg import Float64
 from std_msgs.msg import UInt16MultiArray
 
 from ros_pololu_servo.msg import servo_pololu
+from ros_faceshift.msg import *
 
 class robo_blender :
     def read_motor_config(self, config):
         stream = open(config, 'r')
         self.config = yaml.load(stream)
 
+    def read_input_config(self, config):
+        stream = open(config, 'r')
+        self.inputs = yaml.load(stream)
+        processors = {}
+        source_listeners = {}
+
+        for con in self.inputs:
+            name = con["name"]
+            binding = con["binding"].split(":")
+            source = con["source"].split(":")
+            if con["processor"] not in processors:
+                filename = os.path.join(os.path.dirname(bpy.data.filepath), "processors/%s.py" % con["processor"])
+                exec(compile(open(filename).read(), "processors/%s.py" % con["processor"], 'exec'), globals(), locals())
+                processor = processors[con["processor"]]
+
+            if source[0] not in source_listeners:
+                source_listeners[source[0]] = []
+
+                def call_listeners(msg):
+                    for callback_pair in source_listeners[source[0]]:
+                        listener = callback_pair[0]
+                        con = callback_pair[1]
+                        #print("CALLING %s %s" % (con["source"], con["binding"]))
+                        listener(con, msg)
+
+                print("BINDING %s" % source[0])
+                rospy.Subscriber(source[0], processor.msgclass(), call_listeners)
+
+            def callback(con, msg):
+                self.ops.append([processor, msg, con])
+
+            source_listeners[source[0]].append([callback, con])
+            print("ADDING %s" % con["source"])
+
     def send_motors(self):
         for motor in self.config:
             name = motor["name"]
-            source = motor["source"].split(":")
-            if source[0] == "shapekey":
+            binding = motor["binding"].split(":")
+            if binding[0] == "shapekey":
                 self.position_motor(motor, self.get_shape_position(motor))
-            if source[0] == "bone":
+            if binding[0] == "bone":
                 self.position_motor(motor, self.get_bone_position(motor))
 
     def get_bone_position(self, config):
-        source = config["source"].split(":")
-        bone_parent = source[1]
-        bone = source[2]
-        axis = source[3]
+        binding = config["binding"].split(":")
+        bone_parent = binding[1]
+        bone = binding[2]
+        axis = binding[3]
         cur_pos = self.get_bones_rotation_rad(bone_parent, bone, axis)
         #print("GET BONE: %s -> %s : %s = %s" % (bone_parent, bone, axis, cur_pos))
         return cur_pos
 
     def set_bone_position(self, config, position):
-        source = config["source"].split(":")
-        bone_parent = source[1]
-        bone = source[2]
-        axis = source[3]
-        #print("SET BONE: %s -> %s : %s = %s" % (bone_parent, bone, axis, position))
+        binding = config["binding"].split(":")
+        bone_parent = binding[1]
+        bone = binding[2]
+        axis = binding[3]
+        bpy.data.objects[bone_parent].pose.bones[bone].rotation_mode = 'XYZ'
+        rot = bpy.data.objects[bone_parent].pose.bones[bone].rotation_euler
 
+#        print("AXIS %s" % axis)
+
+        if axis == "x":
+            rot[0] = position * -1
+        if axis == "y":
+            rot[1] = position
+        if axis == "z":
+            rot[2] = position
+        bpy.data.objects[bone_parent].pose.bones[bone].rotation_euler = rot
+        #print("SET BONE: %s -> %s : %s = %s" % (bone_parent, bone, axis, rot))
+        
     def get_shape_position(self, config):
-        source = config["source"].split(":")
-        shape_parent = source[1]
-        shape = source[2]
-        position = 0
+        binding = config["binding"].split(":")
+        shape_parent = binding[1]
+        shape = binding[2]
+        position = bpy.data.meshes[shape_parent].shape_keys.key_blocks[shape].value
         #print("GET SHAPE: %s -> %s = %s" % (shape_parent, shape, position))
         return position
 
     def set_shape_position(self, config, position):
-        source = config["source"].split(":")
-        shape_parent = source[1]
-        shape = source[2]
+        binding = config["binding"].split(":")
+        shape_parent = binding[1]
+        shape = binding[2]
         #print("SET SHAPE: %s -> %s = %s" % (shape_parent, shape, position))
+        bpy.data.meshes[shape_parent].shape_keys.key_blocks[shape].value = position
 
     def position_motor(self, config, angle):
         #print("POSITION %s" % angle)
@@ -76,13 +125,13 @@ class robo_blender :
         msg.acceleration = int(config["acceleration"])
         pub = self.pololus[config["name"]]
         if config["enabled"]: 
-            print("POLOLU: %s @ %s" % (config["name"], angle))
+            #print("POLOLU: %s @ %s" % (config["name"], angle))
             pub.publish(msg)
 
     def position_dynamixel(self, config, angle):
         pub = self.dynamixels[config["name"]]
         if config["enabled"]: 
-            print("DYNAMIXEL: %s @ %s" % (config["name"], angle))
+            #print("DYNAMIXEL: %s @ %s" % (config["name"], angle))
             pub.publish(float(angle))
 
     def get_pose_matrix_in_other_space(self,mat, pose_bone):
@@ -141,6 +190,8 @@ class robo_blender :
         self.dynamixels = {}
         self.pololus = {}
 
+        self.ops = []
+
         namespace = rospy.get_namespace()
         rospy.init_node('robo_blender', anonymous=True)
 
@@ -155,13 +206,25 @@ class robo_blender :
 
         @persistent
         def load_handler(dummy):
+            for op in self.ops:
+                processor = op[0]
+                msg = op[1]
+                con = op[2]
+                binding = con["binding"].split(":")
+                r = processor.process(msg, con)
+                if binding[0] == "shapekey":
+                    self.set_shape_position(con, r)
+                if binding[0] == "bone":
+                    self.set_bone_position(con, r)
+            self.ops = []
             self.send_motors()
 
-        bpy.app.handlers.scene_update_post.append(load_handler)
+        bpy.app.handlers.scene_update_pre.append(load_handler)
 
         print("ROBO: Started")
 
 print("ROBO: Starting")
 robo = robo_blender()
 robo.read_motor_config("motors.yaml")
+robo.read_input_config("inputs.yaml")
 robo.execute()
