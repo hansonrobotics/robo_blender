df3dd4ec27ab4333bacc1c4237a02166a9bfd04e
Linas Vepstas <linasvepstas@gmail.com>
2014-11-26 15:10:21 -0600

    Fix a multitude of different crashes and hangs
    1) sending play, without an animation name in it
    2) sending play, without animation name, as the very first command ever.
    3) sending invalid animation command.

--- src/modes/Animations.py
+++ src/modes/Animations.py
@@ -14,7 +14,7 @@ class Animations:
     controlling anything.
     """
     def __init__(self):
-        #Next playing animation. If not set animation stops
+        # Next playing animation. If not set animation stops
         self.init = False
         self.next = None
         self.command = None
@@ -23,31 +23,50 @@ class Animations:
         self.current = None
 
 
+    # Parse the command string. We are expecting it to be either
+    # play:animation_name, stop or pause
     def parseCommand(self, msg):
         msg = msg.data
         data = msg.split(":", 2)
         command = data[0]
-        #starts playing the animation, resumes current animation.
-        # If animation is playing sets the next animation
+
+        # Start playing the animation, or resume the current animation.
+        # If an animation is playing, then this sets the next animation.
         if command == 'play':
             self.command = 'play'
-            self.next = data[1]
-        #ends animation and stops playing
-        if command == 'stop':
+
+            # The animation name could be empty; that's OK.
+            # Do not clobber the current animation, if a bogus
+            # animation name was provided.
+            if 1 < len(data):
+                if not data[1] in self.animationsList:
+                    print("Error: Unknown animation: " + data[1])
+                else:
+                    self.next = data[1]
+
+        # Halt animation and stops playing
+        elif command == 'stop':
             self.command = 'stop'
             self.next = None
-        #pause animation immediatly
-        if command == 'pause':
-            self.command = 'pause'
 
-    #Sets next animation
+        # Pause animation immediatly
+        elif command == 'pause':
+            self.command = 'pause'
+        else:
+            print("Error: Unsupported command: " + command)
+
+    # Sets next animation
     def _setNext(self):
         self.current = self.next
-        self.anim.setAnimation(self.current)
+
+        # Don't crash if next animation is 'None'
+        if self.current:
+            self.anim.setAnimation(self.current)
 
 
+    # This is called every frame
     def step(self, dt):
-        #Make sure we access bpy data and do other task in blender thread
+        # Make sure we access bpy data and do other task in blender thread
         if not self.init:
             rospy.Subscriber('cmd_animations',String,self.parseCommand)
             self.animationsPub = rospy.Publisher('animations_list', animations_list,None, False,True,None,10)
@@ -57,25 +76,30 @@ class Animations:
             self.init = True
             self.anim.resetAnimation()
 
-        # Parse any pending commands if exists
+        # Parse any pending commands
         if self.command:
             if self.command == 'play':
                 if not self.isPlaying:
                     if not self.current:
                         self._setNext()
-                    self.isPlaying = True
-                    self.anim.playAnimation()
-            if self.command == 'pause':
+                    # If next was null, then, after above, current will
+                    # become null, too.
+                    if self.current:
+                        self.isPlaying = True
+                        self.anim.playAnimation()
+            elif self.command == 'pause':
                 if self.isPlaying:
                     self.anim.stopAnimation()
                     self.isPlaying = False
             self.command = None
 
         if self.isPlaying:
-            #animation finished
+
             rospy.logdebug(self.animationsList[self.current]['length'])
             rospy.logdebug(bpy.context.scene.frame_current)
-            self.anim.playAnimation()
+            # self.anim.playAnimation()
+
+            # Check to see if animation is done
             if bpy.context.scene.frame_current > self.animationsList[self.current]['length']:
                 if self.next:
                     self._setNext()
@@ -85,9 +109,4 @@ class Animations:
                     self.current = None
                     self.anim.resetAnimation()
             outputs.store.full_head.transmit()
-
-
-
-
-
 
